518cc0175e4474a9eaa25fc2b6ef35e6c94ea248
Ade Yahya <adeyahyaprasetyo@gmail.com>
2020-07-26 06:20:52 +0700

    load image from [uint8] instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+#[macro_use]
+pub mod log;
 mod utils;
 
+use image::{DynamicImage, ImageFormat};
+use std::io::{Cursor, Read, Seek, SeekFrom};
+use std::panic;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
-use wasm_bindgen_futures::JsFuture;
-use web_sys::{Request, RequestInit, RequestMode, Response};
 
 #[derive(Debug)]
 struct RgbColor<T> {
@@ -26,23 +28,50 @@ impl RgbColor<u8> {
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+fn load_image_from_array(_array: &[u8]) -> DynamicImage {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    let img = match image::load_from_memory(_array) {
+        Ok(img) => img,
+        Err(error) => {
+            log!("There was a problem opening the file: {:?}", error);
+            panic!("There was a problem opening the file: {:?}", error)
+        }
+    };
+    return img;
+}
+
+fn _get_image_as_array(_img: DynamicImage) -> Vec<u8> {
+    // Create fake "file"
+    let mut c = Cursor::new(Vec::new());
+
+    match _img.write_to(&mut c, ImageFormat::Png) {
+        Ok(c) => c,
+        Err(error) => {
+            log!(
+                "There was a problem writing the resulting buffer: {:?}",
+                error
+            );
+            panic!(
+                "There was a problem writing the resulting buffer: {:?}",
+                error
+            )
+        }
+    };
+    c.seek(SeekFrom::Start(0)).unwrap();
+
+    // Read the "file's" contents into a vector
+    let mut out = Vec::new();
+    c.read_to_end(&mut out).unwrap();
+
+    log!("Sends array back");
+    return out;
+}
+
 #[wasm_bindgen]
-pub async fn get_from_js(url: String) -> Result<JsValue, JsValue> {
+pub fn get_color_palette(_array: &[u8]) -> JsValue {
     utils::set_panic_hook();
-    let mut opts = RequestInit::new();
-    opts.method("GET");
-    opts.mode(RequestMode::Cors);
-
-    // let url = "https://images.unsplash.com/photo-1595446472774-37c5fc18ce46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80";
-    let request = Request::new_with_str_and_init(&url, &opts)?;
-
-    let window = web_sys::window().unwrap();
-    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
-    let resp: Response = resp_value.dyn_into().unwrap();
-    let text = JsFuture::from(resp.array_buffer()?).await?;
-    let image = js_sys::Uint8Array::new(&text);
-    let image = image.to_vec();
-    let colors = dominant_color::get_colors(&image, false);
+    let img = load_image_from_array(_array);
+    let colors = dominant_color::get_colors(&img.to_bytes(), false);
     let mut rgb_colors: Vec<RgbColor<u8>> = Vec::new();
     for n in (2..colors.len()).step_by(3) {
         rgb_colors.push(RgbColor {
@@ -55,5 +84,5 @@ pub async fn get_from_js(url: String) -> Result<JsValue, JsValue> {
         .into_iter()
         .map(|color| color.to_string())
         .collect();
-    Ok(JsValue::from_serde(&rgb_colors).unwrap())
+    JsValue::from_serde(&rgb_colors).unwrap()
 }
